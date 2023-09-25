e59e44752509edb8d1fe224dc218a705b5e07717
izzatillotilavoldiyev5 <izzatillotilavoldiyev5@gmail.com>
2023-09-25 06:34:49 +0500

    Shifo24 project committed

---
+++ src/main/java/com/company/shifo24/Shifo24Application.java
@@ -0,0 +1,13 @@
+package com.company.shifo24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Shifo24Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Shifo24Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/company/shifo24/Shifo24ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.shifo24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Shifo24ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
